e388a2074f5271ec823735bc1e052fa164e81010
wjsc <walterscarano@outlook.com>
2019-08-06 15:04:51 -0300

    updates

--- aws-sns-pubsub/publisher.js
+++ aws-sns-pubsub/publisher.js
@@ -1,11 +1,35 @@
-const TopicArn = 'arn:aws:sns:us-east-1:678806694864:SIMPLE_MESSAGE';
+const TopicArn = 'arn:aws:sns:us-east-1:678806694864:EVENTOS_TEST';
 const AWS = require('aws-sdk');
 AWS.config.loadFromPath('../credentials.json');
 
-const params = {
-  Message: new Date().toString(),
-  TopicArn
-};
+const constructMessageAttributes = (event_id, event_version, emitter_name, emitter_version, client_id) => ({
+  event_id,
+  event_version,
+  emitter_name,
+  emitter_version,
+  date: new Date().toISOString(),
+  client_id
+});
+
+const formatMessageAttributes = obj => Object.keys( obj).reduce( (prev, key) => ({
+  ...prev,
+  ...formatMessageAttribute(key, obj[key], 'String')
+}), {});
+
+const formatMessageAttribute = (key, StringValue, DataType) => ({
+  [key]: {
+    DataType,
+    StringValue
+  }
+});
+
+const constructSNSParams = (TopicArn, Message, ...attributes) => ({
+  Message,
+  TopicArn,
+  MessageAttributes: formatMessageAttributes(constructMessageAttributes(...attributes)),
+});
+
+const params = constructSNSParams(TopicArn, 'ANY_MESSAGE', '1.1', '1.0.3', 'TF-XXX-API', '19.5', 'XXXXX');
 
 const publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
@@ -16,3 +40,35 @@ publishTextPromise.then(
     (err) =>{
     console.error(err, err.stack);
   });
+
+
+// {
+//   Message: 'ANY_MESSAGE',
+//   TopicArn: 'arn:aws:sns:us-east-1:123123123123:EVENTOS_TEST',
+//   MessageAttributes: {
+//       event_id: {
+//           DataType: 'S',
+//           StringValue: '1.1'
+//       },
+//       event_version: {
+//           DataType: 'S',
+//           StringValue: '1.0.3'
+//       },
+//       emitter_name: {
+//           DataType: 'S',
+//           StringValue: 'TF-XXX-API'
+//       },
+//       emitter_version: {
+//           DataType: 'S',
+//           StringValue: '19.5'
+//       },
+//       date: {
+//           DataType: 'S', 
+//           StringValue: '2019-08-06T17:45:45.538Z'
+//       },
+//       client_id: {
+//           DataType: 'S',
+//           StringValue: 'XXXXX'
+//       }
+//   }
+// }
--- aws-sns-pubsub/subscriber.js
+++ aws-sns-pubsub/subscriber.js
@@ -6,9 +6,9 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.text());
 
-const TopicArn = 'arn:aws:sns:us-east-1:678806694864:SIMPLE_MESSAGE';
+const TopicArn = 'arn:aws:sns:us-east-1:678806694864:EVENTOS_TEST';
 const Protocol = 'http';
-const Endpoint = 'http://ec2-18-205-24-250.compute-1.amazonaws.com/confirm';
+const Endpoint = 'http://ec2-54-235-238-92.compute-1.amazonaws.com/receive';
 
 
 const subscribe = (sns, Protocol, Endpoint, TopicArn) => {
@@ -19,10 +19,10 @@ const confirm = (sns, Token, TopicArn, AuthenticateOnUnsubscribe) => {
   return sns.confirmSubscription({Token, TopicArn, AuthenticateOnUnsubscribe}).promise();
 }
 
-
 app.get('/subscribe', async (req, res) => {
   try{
     const result = await subscribe(sns, Protocol, Endpoint, TopicArn);
+    console.log(result);
     res.send(result);
   }
   catch(error){
@@ -31,9 +31,10 @@ app.get('/subscribe', async (req, res) => {
   }
 });
 
-app.post('/confirm', async (req, res) => {
+app.post('/receive', async (req, res) => {
   try{
     const body = JSON.parse(req.body);
+    console.log(body);
     if(body.Token){
         const result = await confirm(sns, body.Token, TopicArn, 'false');
     }
@@ -46,3 +47,42 @@ app.post('/confirm', async (req, res) => {
 })
  
 app.listen(80);
+
+
+// {
+//   Type: 'Notification',
+//   MessageId: '806b9aef-f87a-56d5-bdfe-cf2c3aeda8b6',
+//   TopicArn: 'arn:aws:sns:us-east-1:678806694864:EVENTOS_TEST',
+//   Message: 'ANY_MESSAGE',
+//   Timestamp: '2019-08-06T18:00:55.247Z',
+//   SignatureVersion: '1',
+//   Signature: 'TGAEy7glm0AYLUmuZhYOZ6NMdZGztIDZFuvnv3q9si/c6xgCRks5ZS4INl/DDoy/usuZgfsULdSwdK4pibVroGwnUnixlHrD5ByMNb9pk/uTxuxmljhzfnH2xIornIl54N11aENY3XNFl30SW7LBbpOKUN/lcjjbYYjyr0e/gGX3qj96Sy7mAOme2ojLaNAk7ExxH21q+PNI1CIh5/5WOfwYK0sUUjnwPwx+ASTPHKbO/GnH12C08WOC3IeBuT9Ad9gDSZbwk2aZmFH2Eh++Ar5Yy6d+MRAY4lOuBx3uOd37JVyrWdKj8MPlOWrL3ulaXpLq4xPrOvlnHPp7plRIqQ==',
+//   SigningCertURL: 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-6aad65c2f9911b05cd53efda11f913f9.pem',
+//   UnsubscribeURL: 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:678806694864:EVENTOS_TEST:3b9c0905-c2fd-4809-8a27-fe16edf93379',
+//   MessageAttributes: {
+//       event_version: {
+//           Type: 'String',
+//           Value: '1.0.3'
+//       },
+//       emitter_name: {
+//           Type: 'String',
+//           Value: 'TF-XXX-API'
+//       },
+//       date: {
+//           Type: 'String',
+//           Value: '2019-08-06T18:00:54.274Z'
+//       },
+//       emitter_version: {
+//           Type: 'String',
+//           Value: '19.5'
+//       },
+//       event_id: {
+//           Type: 'String',
+//           Value: '1.1'
+//       },
+//       client_id: {
+//           Type: 'String',
+//           Value: 'XXXXX'
+//       }
+//   }
+// }
